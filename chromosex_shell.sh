b93a3f8e24f3a5fefedd38e7432caf09d789d878
lnpott9 <74425544+lnpott9@users.noreply.github.com>
2025-01-27 14:30:10 -0600

    Create chromosex_shell.sh

---
+++ chromosex_shell.sh
@@ -0,0 +1,28 @@
+# shell script to be paired with chromosex.r for processing BAM files
+
+# 1. copy all deduplicated bam files to input folder
+
+# 2. designate input folder
+input_folder="<path/to/input/folder>"
+
+# 3. designate output file name
+output_summary_file="<outputfilename.txt>"
+
+# 4. load R and samtools
+module load R 
+module load samtools
+
+# 5. loop through all bam files and convert them to idxstats, then run the idxstat file through the R script
+for bam_file in "$input_folder"/*.bam; do
+	bai_file="${bam_file%.bam}.bai"
+	if [ ! -f "$bai_file" ]; then
+		samtools index "$bam_file"		
+	fi
+	output_file="${bam_file%.bam}.idxstats"
+	samtools idxstats "$bam_file" > "$output_file"
+	Rscript chromosex.r "$output_file"
+done
+
+# 6. remove intermediate bai, bam, and idxstats files
+rm $input_folder/*idxstats
+rm $input_folder/*.bam*
